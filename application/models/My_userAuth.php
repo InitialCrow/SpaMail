e7effd855b4c3a9d85fde5afff18ed7de5682cbd
Parent Adam <parent.adam@free.fr>
2016-07-20 16:39:08 +0200

    fix minor bug

--- application/models/My_userAuth.php
+++ application/models/My_userAuth.php
@@ -6,7 +6,7 @@
 	   $this -> db -> select('id, identifiant, mdp, signature');
 	   $this -> db -> from('utilisateur');
 	   $this -> db -> where('identifiant', $identifiant);
-	   $this -> db -> where('mdp', $mdp);
+	   $this -> db -> where('mdp', intval($mdp));
 
 	   $this -> db -> limit(1);
 	 
