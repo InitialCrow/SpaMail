9ec196607a631df3fc07ec3434de037733887e4d
Parent Adam <parent.adam@free.fr>
2016-07-20 09:32:38 +0200

    Update config_mail.php

--- config_mail.php
+++ config_mail.php
@@ -1,23 +1,23 @@
 <?php 
 
 // smtp host
-define("SMTP_HOST","courrier.kertios.com");
+define("SMTP_HOST","******");
 
 //smtp Port
-define("SMTP_PORT", 587);
+define("SMTP_PORT", ****);
 
 // smtp authentification
 define("SMTP_AUTH",true);
 
 
 // smtp auth_logs
-define("SMTP_USERNAME","adam.parent@kertios.com");
-define("SMTP_PASSWORD","2df030");
+define("SMTP_USERNAME","*******");
+define("SMTP_PASSWORD","*******");
 
 // seed / migrate dataBase
 define('DATABASE_HOST','localhost');
 define('DATABASE_USERNAME','root');
 define('DATABASE_PASS','');
-define('DATABASE_NAME','mailing_kertios');
+define('DATABASE_NAME','*******');
 
 ?>
